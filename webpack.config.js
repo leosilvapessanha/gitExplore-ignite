4f7a8344cae6d1fdf991382be84cc11b285fdcad
Léo <leonardosilvapessanha@gmail.com>
2021-03-10 12:31:32 -0300

    first module finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 const path = require('path')
+const HtmlWebpackPlugin=require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports ={
   entry: path.resolve(__dirname,'src', 'index.jsx') ,
+  devtool: 'eval-source-map',
   output: {
     path: path.resolve(__dirname,'dist'),
     filename: 'bundle.js'
   },
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devServer: {
+    contentBase: path.resolve(__dirname,'public')
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:path.resolve(__dirname,'public','index.html')
+    })
+  ],
   module:{
     rules:[
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader'
-      }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ]
   },
 }
